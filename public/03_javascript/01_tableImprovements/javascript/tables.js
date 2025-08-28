e5bcbef248d1d2c3eabd932c6276c94e42709490
Res Kocher <res.kocher@digitecgalaxus.ch>
2025-08-28 17:47:57 +0200

    example to simplify code

--- public/03_javascript/01_tableImprovements/javascript/tables.js
+++ public/03_javascript/01_tableImprovements/javascript/tables.js
@@ -2,6 +2,8 @@ document.querySelectorAll("[data-table-name]").forEach((element) => {
   const tableType = element.getAttribute("data-table-name");
   if (tableType === "benchmark") {
     const columns = element.querySelectorAll("[data-table-column]");
+
+    // working but not optimized solution
     let columnWidest = 0;
     columns.forEach((col) => {
       const colWidth = Number(col.innerText);
@@ -11,8 +13,17 @@ document.querySelectorAll("[data-table-name]").forEach((element) => {
     });
     columns.forEach((col) => {
       const colWidth = col.innerText;
-      const width = 100 / columnWidest * colWidth;
+      const width = (100 / columnWidest) * colWidth;
       col.style.width = `${width}%`;
     });
+
+    // optimized solution but also not easy to understand on first glance
+    /*     const maxWidth = Math.max(
+      ...Array.from(columns, (col) => Number(col.innerText))
+    );
+    columns.forEach((col) => {
+      col.style.width = `${(100 * Number(col.innerText)) / maxWidth}%`;
+    });
+ */
   }
 });
